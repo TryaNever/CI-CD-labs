eb1aa3e035d206dc45b58d7eec9e500e2c17e042
TryaNever <alexispernette@outlook.fr>
2026-04-23 13:27:18 +0200

    ADD NavBar test

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,23 +1,54 @@
 import js from "@eslint/js";
 import globals from "globals";
 import react from "eslint-plugin-react";
+import babelParser from "@babel/eslint-parser";
+import jest from "eslint-plugin-jest";
 
 export default [
   js.configs.recommended,
-
+  {
+    files: ["**/*.{test,spec}.{js,jsx}", "**/__tests__/**/*.{js,jsx}"],
+    plugins: {
+      jest,
+    },
+    languageOptions: {
+      globals: {
+        ...globals.jest,
+      },
+    },
+    rules: {
+      ...jest.configs.recommended.rules,
+    },
+  },
   {
     files: ["**/*.{js,jsx}"],
     languageOptions: {
-      globals: globals.browser,
+      parser: babelParser,
+      globals: {
+        ...globals.browser,
+      },
       parserOptions: {
+        requireConfigFile: false,
+        babelOptions: {
+          presets: ["@babel/preset-react"],
+        },
         ecmaFeatures: {
           jsx: true,
         },
+        sourceType: "module",
       },
     },
+
     plugins: {
       react,
     },
+
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+
     rules: {
       "react/react-in-jsx-scope": "off",
       "react/jsx-uses-vars": "error",
